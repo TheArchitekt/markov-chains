e1436bfd349d1e7b91cac2d30c5afc5119afe883
TheArchitekt <richardpennymon@gmail.com>
2022-11-13 14:55:32 -0500

    Update Markov Chains

--- markov.py
+++ markov.py
@@ -1,5 +1,10 @@
 """Generate Markov text from text files."""
 
+#First, you’ll create a Python script to procedurally generate original
+#content based on an excerpt from Green Eggs and Ham by Dr. Seuss. Then,
+#you’ll enhance your script so that it works on any corpus (any existing piece of text)
+#and even combinations of corpuses.
+
 from random import choice
 
 
@@ -10,9 +15,11 @@ def open_and_read_file(file_path):
     the file's contents as one string of text.
     """
 
-    # your code goes here
+    #open input text file
+    contents = open('green-eggs.txt').read()
 
-    return 'Contents of your file as one long string'
+    #return the contents
+    return contents
 
 
 def make_chains(text_string):
@@ -40,19 +47,66 @@ def make_chains(text_string):
         [None]
     """
 
+
+    #break up input text
+        #add rstrip is spacing is weird
+    words = text_string.split()
+
+    #initialize empty dictionary
     chains = {}
 
-    # your code goes here
+    #create for loop to parse through input text to find words & -1 to keep in a parameter
+    for word in range(len(words) - 2):
 
+        #turn words into the dictionary structure
+        chains_key = (words[word], words[word + 1])
+        #print(chains_key)
+
+        #assign variable to list
+        chains_value = words[word + 2]
+        #print(chains_value)
+
+        #create condition in the event that the word isn't the dictionary
+        if chains_key not in chains:
+            chains[chains_key] = []
+
+        #add the chain value to the chain key
+        chains[chains_key].append(chains_value)
+
+    #print(chains)
+
+    #return
     return chains
+
+
 
 
 def make_text(chains):
     """Return text from chains."""
 
+    #initialize empty words list to return words into
     words = []
 
-    # your code goes here
+    #use random’s choice function to pick a random key from chains.keys()
+    #and use the list() constructor to convert it into a list
+    chosen_words = choice(list(chains.keys()))
+    chosen_keys = [chosen_words[0], chosen_words[1]]
+
+    between = choice(chains[chosen_words])
+
+    #create a for loop to parse through chains
+    #parse through our 2 chosen keys?
+    for chosen_words in chosen_keys:
+        words.append(chosen_words)
+
+    print(between)
+
+    #chosen_word[1] = choice(list(words.keys()))
+
+
+    print(chosen_keys)
+    # print(chosen_word[1])
+
 
     return ' '.join(words)
 
